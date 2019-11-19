3a8877aad307d5193afd553080ecf7cf99ada4f6
Jerd <teeo300881@gmail.com>
2019-11-19 16:51:17 +0700

    complete project

---
+++ ComponanceLabel/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ComponanceLabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(517, 378);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(525, 33);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(558, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(558, 71);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 266);
+            this.textBox1.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Location = new System.Drawing.Point(558, 340);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(240, 38);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(558, 381);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 57);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Print Barcode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(209, 212);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Camera Off";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 454);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Componance Label";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ComponanceLabel/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BarcodeLib;
+using Dynamsoft.Barcode;
+using TouchlessLib;
+
+namespace ComponanceLabel
+{
+    public partial class Form1 : Form
+    {
+        private BarcodeReader _barcodeReader;
+        private TouchlessMgr _touch;
+        private const int _previewWidth = 640;
+        private const int _previewHeight = 360;
+        private Barcode _b;
+
+        Bitmap MemoryImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _barcodeReader = new BarcodeReader();
+            _touch = new TouchlessMgr();
+
+            _b = new Barcode();
+
+            _b.ImageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+            _b.IncludeLabel = true;
+            _b.LabelFont = new Font("Microsoft Himalaya", 9, FontStyle.Regular);
+            _b.LabelPosition = LabelPositions.BOTTOMCENTER;
+            _b.Alignment = AlignmentPositions.CENTER;
+            _b.RotateFlipType = RotateFlipType.RotateNoneFlipNone;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = _touch.Cameras;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string button_text = button1.Text;
+            if (button_text.Equals("Start Scan"))
+            {
+                label1.Visible = false;
+                button2.Enabled = false;
+                button1.Text = "Stop Scan";
+                StartCamera();
+            }
+            else
+            {
+                button1.Text = "Start Scan";
+                StopCamera();
+                label1.Visible = true;
+                button2.Enabled = true;
+            }
+
+
+        }
+
+        private void StopCamera()
+        {
+            label1.Visible = true;
+            button1.Text = "Start Scan";
+            if (_touch.CurrentCamera != null)
+            {
+                _touch.CurrentCamera.OnImageCaptured -= new EventHandler<CameraEventArgs>(OnImageCaptured);
+
+            }
+        }
+        private void StartCamera()
+        {
+            if (comboBox1.Text == null)
+            {
+                MessageBox.Show("No USB webcam connected Or Select Device");
+                button1.Text = "Start Scan";
+                return;
+            }
+
+            // Start to acquire images
+
+            _touch.CurrentCamera = _touch.Cameras[comboBox1.SelectedIndex];
+            _touch.CurrentCamera.CaptureWidth = _previewWidth; // Set width
+            _touch.CurrentCamera.CaptureWidth = _previewHeight; // Set height
+            _touch.CurrentCamera.OnImageCaptured += new EventHandler<CameraEventArgs>(OnImageCaptured); // Set preview callback function
+        }
+
+        private void OnImageCaptured(object sender, CameraEventArgs args)
+        {
+            // Get the bitmap
+            Bitmap bitmap = args.Image;
+
+            // Read barcode and show results in UI thread
+            this.Invoke((MethodInvoker)delegate
+            {
+                pictureBox1.Image = bitmap;
+                ReadBarcode(bitmap);
+            });
+        }
+
+        private void ReadBarcode(Bitmap bitmap)
+        {
+
+            try
+            {
+
+                // Read barcodes with Dynamsoft Barcode Reader
+                Stopwatch sw = Stopwatch.StartNew();
+                sw.Start();
+                BarcodeResult[] results = _barcodeReader.DecodeBitmap(bitmap);
+                sw.Stop();
+                Console.WriteLine(sw.Elapsed.TotalMilliseconds + "ms");
+
+                // Clear previous results
+                textBox1.Clear();
+
+                if (results == null)
+                {
+                    textBox1.Text = "No barcode detected!";
+                    pictureBox2.Image = null;
+                    return;
+                }
+
+                String[] strlist = results[0].BarcodeText.Split(',');
+
+                Array.Resize(ref strlist, strlist.Length - 1);
+
+                // Display barcode results
+                string newLine = Environment.NewLine;
+
+                for (int i = 0; i <= 6; i++)
+                {
+                    textBox1.AppendText(strlist[i] + newLine);
+
+                }
+
+                if (strlist == null)
+                {
+                    return;
+                }
+
+                Image img = _b.Encode(TYPE.CODE39, strlist[1].ToString(), Color.Black, Color.Transparent, 200, 20);
+
+                pictureBox2.Image = img;
+
+                button2.Enabled = true;
+
+                StopCamera();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                StopCamera();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetPrintArea(pictureBox2);
+            printDocument1.Print();
+        }
+        public void GetPrintArea(PictureBox pnl)
+        {
+            MemoryImage = new Bitmap(pnl.Width, pnl.Height);
+            Rectangle rect = new Rectangle(0, 0, pnl.Width, pnl.Height);
+            pnl.DrawToBitmap(MemoryImage, new Rectangle(0, 0, pnl.Width, pnl.Height));
+        }
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Rectangle pagearea = e.PageBounds;
+            e.Graphics.DrawImage(MemoryImage, 0, 0);
+        }
+    }
+}
